1c192dabaebeadda810d03c4d1026e3e0e952a34
Gonzalo Scanferla <gonzalo.scanferla@gmail.com>
2024-03-05 18:00:17 +0100

    Refactorización de los botones de navegación entre paginas pasando toda al funcionalidad al html

--- src/app/pages/users-list/users-list.component.ts
+++ src/app/pages/users-list/users-list.component.ts
@@ -17,8 +17,6 @@ export class UsersListComponent {
   arrUsers : IUser [] = []
   page : number = 1
   totalPages : number = 1
-  isDisabledPrev : boolean = true
-  isDisabledNext : boolean = true
 
   // Importación con Observables.
   // una vez importados los datos verifico el numero de paginas para activar/desactivar la navegación entre páginas
@@ -27,29 +25,7 @@ export class UsersListComponent {
       this.arrUsers = data.results
       this.page = data.page
       this.totalPages = data.total_pages
-      this.isDisabledNext = (this.page === this.totalPages) ? true : false
     } )
-  }
-  
-  
-  // Función para activar y desactivar los botones prev y next en función del numero de páginas y pagina actual
-  changePage ($event:any) {
-    if ($event.target.value === "prev"){
-      this.page --
-      this.usersServices.getAll(this.page).subscribe((data: IData) => {
-        this.arrUsers = data.results})
-        this.isDisabledPrev = (this.page === 1) ? true : false
-        this.isDisabledNext = (this.page != this.totalPages) ? false : true
-    }
-    if ($event.target.value === "next"){
-      this.page ++
-
-      this.usersServices.getAll(this.page).subscribe((data: IData) => {
-        this.arrUsers = data.results})
-
-      this.isDisabledPrev = false
-      this.isDisabledNext = (this.page === this.totalPages) ? true : false
-    }
   }
 
   // Importación con promesas
