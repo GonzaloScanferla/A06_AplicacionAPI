84b9a89d1b738c943789749de3eae246b1accc0f
Gonzalo Scanferla <gonzalo.scanferla@gmail.com>
2024-03-08 11:44:33 +0100

    En el servicio se corrige el httpCCclient a private

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -9,33 +9,33 @@ import { IData } from '../interfaces/idata.interface';
 })
 export class UsersService {
 
-  HttpClient = inject(HttpClient)
+  private httpClient = inject(HttpClient)
   url = 'https://peticiones.online/api/users'
   // Importo los datos con observables
 
   getAll(page : number): Observable <IData> {
-    return this.HttpClient.get<IData>(`${this.url}?page=${page}`)
+    return this.httpClient.get<IData>(`${this.url}?page=${page}`)
   }
 
   // // Importación con promesas
   // getAllPromises(): Promise<IUser[]> {
-  //   return lastValueFrom (this.HttpClient.get<IUser[]>(this.urlBase))
+  //   return lastValueFrom (this.httpClient.get<IUser[]>(this.urlBase))
   // }
 
   getById (_id : string): Observable <IUser> {
-    return this.HttpClient.get<IUser>(`${this.url}/${_id}`)
+    return this.httpClient.get<IUser>(`${this.url}/${_id}`)
   }
 
   deleteUser (_id: string) : Observable <IUser> {
-    return this.HttpClient.delete<IUser> (`${this.url}/${_id}`)
+    return this.httpClient.delete<IUser> (`${this.url}/${_id}`)
   }
 
   createNewUser (formValue: IUser) : Observable <IUser> {
-    return this.HttpClient.post <IUser> (`${this.url}`,formValue)
+    return this.httpClient.post <IUser> (`${this.url}`,formValue)
   }
 
   updateUser (formValue: IUser) : Observable <IUser> {
-    return this.HttpClient.put <IUser> (`${this.url}/${formValue._id}`,formValue)
+    return this.httpClient.put <IUser> (`${this.url}/${formValue._id}`,formValue)
   }
 
 }
